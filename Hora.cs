ca1d8df2595c938d4de011a86b59b9ff07b315ce
ri.souza <ri.souza@GPLDBNW10193>
2022-06-10 08:39:07 -0300

    Add project files.

---
+++ Hora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcularHoras
+{
+    public class Hora
+    {
+        public int hora { get; set; }
+        public int min { get; set; }
+        public int seg { get; set; }
+
+        public TimeSpan horas { get; set; }
+
+        public void PedirHora(string h, string m)
+
+        {
+            seg = 0;
+
+            Console.Write($"\n\nEntre com a HORA {h}...:");
+            hora = int.Parse(Console.ReadLine());
+
+            Console.Write($"\nEntre com os MINUTOS {m}....:");
+            min = int.Parse(Console.ReadLine());
+
+            horas = new TimeSpan(hora, min, seg);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalcularHoras
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos calcular o tempo decorrido\n\n");
+
+            Hora hInicial = new Hora();
+            hInicial.PedirHora("INICIAL", "INICIAIS");
+            TimeSpan inicio = hInicial.horas;
+
+            Hora hFim = new Hora();
+            hFim.PedirHora("FINAL", "FINAIS");
+            TimeSpan fim = hFim.horas;
+
+            TimeSpan intervalo = fim - inicio;
+
+            Console.WriteLine($"\n\n\nO intervalo de tempo ...: {intervalo.ToString()}");
+
+            Console.ReadLine();
+        }
+    }
+}
